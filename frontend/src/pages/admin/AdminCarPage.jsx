8357a2795dfc35cec07f12ba167d279c2e3196bd
unclejambo <jajacampos64@gmail.com>
2025-08-21 01:32:45 +0800

    frontend balik

--- frontend/src/pages/admin/AdminCarPage.jsx
+++ frontend/src/pages/admin/AdminCarPage.jsx
@@ -33,7 +33,6 @@ export default function AdminCarPage() {
   const closeAddModal = () => setShowAddModal(false);
   const closeEditModal = () => setEditCar(null);
 
-  // Get the appropriate data and columns based on active tab
   const {
     data,
     columns,
